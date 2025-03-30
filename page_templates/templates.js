0d8f47b5e1de48a29623a00d35b77127c38f9f3d
Ciarán McGann <ciaranjmcgann@gmail.com>
2025-03-30 17:03:25 +0100

    Feature 008 nav, header and footer templates created and using js

---
+++ page_templates/templates.js
@@ -0,0 +1,15 @@
+function loadHTML(elementId, file) {
+  fetch(file)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(elementId).innerHTML = data;
+      })
+      .catch(error => console.error(`Error loading ${file}:`, error));
+}
+
+// Load components on every page
+document.addEventListener("DOMContentLoaded", function () {
+  loadHTML("navbar", "/page_templates/navbar.html");
+  loadHTML("header", "/page_templates/header.html");
+  loadHTML("footer", "/page_templates/footer.html");
+});
