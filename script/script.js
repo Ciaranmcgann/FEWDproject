c5329eede471af2c7d8ffc7a77027120dd5def83
Ciarán McGann <ciaranjmcgann@gmail.com>
2025-04-08 19:41:05 +0100

    Feature 013 fixed href file paths in navbar

--- script/script.js
+++ script/script.js
@@ -21,24 +21,6 @@ function searchFunction() {
   }
 }
 
-// Open street map script
-
-  document.addEventListener("DOMContentLoaded", function () {
-    // sets view of map to dublin
-    var map = L.map('map').setView([53.34399, -6.24856], 13);
-
-    // Add OpenStreetMap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // marker for McGann's Music location
-    L.marker([53.3431, -6.2626]).addTo(map)
-        .bindPopup("McGann's Music Shop")
-        .openPopup();
-});
-
-
 
 
 
