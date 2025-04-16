e4f120d0b2da6f14de2b6b66973de07543ba3dcb
Ciarán McGann <ciaranjmcgann@gmail.com>
2025-04-16 14:59:12 +0100

    test

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,6 @@
 <a href="#/" class="HeaderlinkHome">
   <header class="text-white text-center py-5">
     <h1 class="hero">McGann's Music Shop</h1>
-    <p>For all your music desires!!! testn</p>
+    <p>For all your music desires!!! test</p>
   </header>
 </a>
